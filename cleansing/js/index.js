c3f26d71e823974b8b776e4473959d3c81a38a64
GIT-DingJinJiang <1643814124@qq.com>
2021-10-14 16:24:38 +0800

    2021/10/14/16:24

--- cleansing/js/index.js
+++ cleansing/js/index.js
@@ -1,3 +1,14 @@
+//无级变速
+window.addEventListener('load', function () {
+    var em = () => {
+        let width = document.documentElement.clientWidth;/*获取设备的宽度*/
+        // 16和640假定数据可随意更改
+        let size = (16 * (width / 640) > 40 ? 40 + "px" : (16 * (width / 640) + "px"))
+        document.documentElement.style.fontSize = size;
+    }
+    window.addEventListener("load", em);//初始化
+    window.addEventListener("resize", em)/*屏幕发生改变时调佣函数*/
+})
 //<!-- 导航栏 -->
 var nav = document.querySelector(".list_rights");
 // 节流阀
