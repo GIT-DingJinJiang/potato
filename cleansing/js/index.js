573af823b50a16cf393ff6a19a0f82b33b8803e5
GIT-DingJinJiang <1643814124@qq.com>
2021-10-16 13:11:44 +0800

    2021/10/16/中午

--- cleansing/js/index.js
+++ cleansing/js/index.js
@@ -1,29 +1,29 @@
 //无级变速
-window.addEventListener('load', function () {
-    var em = () => {
-        let width = document.documentElement.clientWidth;/*获取设备的宽度*/
-        // 16和640假定数据可随意更改
-        let size = (16 * (width / 640) > 40 ? 40 + "px" : (16 * (width / 640) + "px"))
-        document.documentElement.style.fontSize = size;
-    }
-    window.addEventListener("load", em);//初始化
-    window.addEventListener("resize", em)/*屏幕发生改变时调佣函数*/
-})
-//<!-- 导航栏 -->
+window.addEventListener('load', function() {
+        var em = () => {
+            let width = document.documentElement.clientWidth; /*获取设备的宽度*/
+            // 16和640假定数据可随意更改
+            let size = (16 * (width / 640) > 40 ? 40 + "px" : (16 * (width / 640) + "px"))
+            document.documentElement.style.fontSize = size;
+        }
+        window.addEventListener("load", em); //初始化
+        window.addEventListener("resize", em) /*屏幕发生改变时调佣函数*/
+    })
+    //<!-- 导航栏 -->
 var nav = document.querySelector(".list_rights");
 // 节流阀
 var flag = true;
 nav.addEventListener('click', () => {
-    if (flag) {
-        nav.className = 'list_rights active';
-        flag = false;
-    } else {
-        nav.className = 'list_rights';
-        flag = true;
-    }
-})
-// 轮播
-window.addEventListener('load', function () {
+        if (flag) {
+            nav.className = 'list_rights active';
+            flag = false;
+        } else {
+            nav.className = 'list_rights';
+            flag = true;
+        }
+    })
+    // 轮播
+window.addEventListener('load', function() {
     var swiper = new Swiper('.swiper-container', {
         spaceBetween: 30,
         centeredSlides: true,
@@ -41,10 +41,3 @@ window.addEventListener('load', function () {
         }
     });
 })
-
-
-
-
-
-
-
